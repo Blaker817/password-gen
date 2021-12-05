53beb18ba18e7ad8bb771efde48c12c0630f80f1
Blake Elliott <blakeelliott@Blakes-MacBook-Pro.local>
2021-12-05 16:50:30 -0600

    added updated Readme.md file

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # Password Generator Starter Code
 
 ## Description
+Made a clickable button that genrates a password between 8 and 128 characters usin uppercase,lowercase,numbers, and special characters.
 
 ## GitHub Link:
+
+
+## URL:
+http://localhost:52330/challenges/password-gen/index.html
 
 ## Screenshot:
--- script.js
+++ script.js
@@ -1,68 +1,59 @@
-// Assignment code here
-// const rangeOfCharacters = document.getElementById
-// ('rangeOfCharacters')
-// const rangeOfNumbers = document.getElementById
-// ('rangeOfNumbers')
-
-// rangeOfCharacters.addEventListener('input', syncRangeAmount)
-// rangeOfNumbers.addEventListener('input', syncRangeAmount)
-
-// Function syncRangeAmount(e) {
-//   const value = e.target.value
-//   rangeOfCharacters.value = value
-//   rangeOfNumbers.value = value
-// }
-
-
-
-
-// Get references to the #generate element
+// creating variables to hold all characters
 var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var lower = "abcdefghijklmnopqrstuvwxyz"
 var numbers = "1234567890"
 var special = "'!@#$%^&*()_+{}:<>?/[]'"
 
+// refering to generate button 
 var generateBtn = document.querySelector("#generate");
 
+// defining generatePassword function
 function generatePassword() {
+  // creating empty variables within the scope of this function
   var password = ''
   var passwordCharacterHolder = ''
 
+  // prompting user for a length
   var pwdLength = prompt("Between 8 and 128 how many characters would you like your password to contain?")
-
+  // checking to make sure length is between 8 and 128 characters
   while (pwdLength < 8 || pwdLength > 128) {
     alert('Password must be a number between 8 and 128')
     pwdLength = prompt("Between 8 and 128 how many characters would you like your password to contain?")
   }
-
-  var wantsUpper = confirm('would you like uppercase letters?')
-  var wantsLower = confirm('would you like lowercase letters?')
-  var wantsNumbers = confirm('would you like numbers?')
-  var wantsSpecial = confirm('would you like special characters?')
-
+  // Comfirming that we are using Uppercase, Lowercase, Numbers, and Special Characters 
+  var wantsUpper = confirm('Would you like uppercase letters?')
+  var wantsLower = confirm('Would you like lowercase letters?')
+  var wantsNumbers = confirm('Would you like numbers?')
+  var wantsSpecial = confirm('Would you like special characters?')
+// ALert conformation that you must choose one character type
   while (!wantsUpper && !wantsLower && !wantsNumbers && !wantsSpecial) {
     alert('You must choose at least one character type')
-    wantsUpper = confirm('would you like uppercase letters?')
-    wantsLower = confirm('would you like lowercase letters?')
-    wantsNumbers = confirm('would you like numbers?')
-    wantsSpecial = confirm('would you like special characters?')
+    wantsUpper = confirm('Would you like uppercase letters?')
+    wantsLower = confirm('Would you like lowercase letters?')
+    wantsNumbers = confirm('Would you like numbers?')
+    wantsSpecial = confirm('Would you like special characters?')
   }
-
-  if(wantsUpper) {
+  // if they want a certain character then add it to the passwordCharacterHolder variable
+  if (wantsUpper) {
     passwordCharacterHolder += upper
   }
-  if(wantsLower) {
+  if (wantsLower) {
     passwordCharacterHolder += lower
   }
-  if(wantsNumbers) {
+  if (wantsNumbers) {
     passwordCharacterHolder += numbers
   }
-  if(wantsSpecial) {
+  if (wantsSpecial) {
     passwordCharacterHolder += special
   }
 
   console.log(passwordCharacterHolder)
-
+  // for loop is running the code for as many times as the length is that the user has chosen
+  for (var i = 0; i < pwdLength; i++) {
+    // adding a character at random from our passwordcharacterholder into our password variable
+    password += passwordCharacterHolder.charAt(Math.floor(Math.random() * passwordCharacterHolder.length))
+  }
+  // returning our new password
   return password
 }
 
